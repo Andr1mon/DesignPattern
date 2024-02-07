d34bcb2c8f444bf069b0a8b468260aae27cea184
Andrei <andr1mpn@gmail.com>
2024-02-07 15:20:39 +0100

    Initial commit

---
+++ src/test/java/fr/ensim/dp/cache/MemoryCacheTest.java
@@ -0,0 +1,35 @@
+package fr.ensim.dp.cache;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MemoryCacheTest {
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAll() {
+		// cle
+		// Size vaut0
+		// byte [] b = {12,2 ,3} ,
+		// add("key1", b)
+		// Size vaut 3
+		//retreive 
+		
+	}
+	@Test
+	public void testRetreive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClear() {
+		fail("Not yet implemented");
+	}
+
+}
